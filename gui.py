ae2ae8c02ad0f731f0fcf59addc3cc33c56509cd
Nada-Hany <nadaaayman993@gmail.com>
2024-10-11 17:22:59 +0300

    graph window

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
 from utils import Button
+from utils import Signal
+import utils
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 # Input Labels and Fields
@@ -14,27 +18,82 @@ positions = [
     (60, 200), (300, 200) # Row 3
 ]
 
-#left section for all buttons
-def left_section(root):
+def to_generate_signal(rightFrame):
+    frame = right_frame(rightFrame)
+    generate_signal_input(frame)
+    print("to_generate_signal btn triggered")
+
+#left section for all buttons - right section for displaying 
+def sections(root):
     left_frame = tk.Frame(root, width=150, height=500, bg="#d3d3d3")
     left_frame.pack(side="left", fill="y")
+    left_frame.pack_propagate(False)
 
-    generate_signal_btn = Button(20, 40, "Generate Signal")
+    right_frame = tk.Frame(root, width=450, height=500, bg="white", highlightbackground='black', highlightthickness=2)
+    right_frame.pack(side="right", fill="both", expand=True)
+    right_frame.pack_propagate(False)
+
+    generate_signal_btn = Button(20, 40, "Generate Signal", lambda:to_generate_signal(right_frame))
     buttons = []
     buttons.append(generate_signal_btn)
     for btn in buttons:
-        tmp = tk.Button(left_frame, text=f"{btn.name}", bg="#808080", fg="white", width=15, height=2)
+        tmp = tk.Button(left_frame, text=f"{btn.name}", bg="#808080", fg="white", width=15, height=2, command=btn.onClick)
         tmp.place(x=btn.x, y=btn.y)
+    
+    return right_frame
   
 
-#entrying values for generating a signal window
-def generate_signal(root):
-    right_frame = tk.Frame(root, width=450, height=500, bg="white")
-    right_frame.pack(side="right", fill="both", expand=True)
-
-    frame = tk.Frame(right_frame, width=520, height=480, bg="#d3d3d3")  # Darker frame color
+def right_frame(root):
+    frame = tk.Frame(root, width=520, height=480, bg="#d3d3d3")  # Darker frame color
     frame.place(relx=0.5, rely=0.5, anchor="center")  # Center the frame
 
+    return frame
+
+def display_graph(error_lbl, old_frame, root):
+    error_lbl.place(x=900, y=300)
+    #valid inputs -> construct signal obj and go to displaying the graphs
+    if(utils.valid_inputs(entries, error_lbl)):
+        old_frame.destroy()
+        frame = right_frame(root)
+        # test = tk.Label(frame, text="in graph window")
+        # test.place(x=200,y=300)
+
+        # dummy graph
+        fig = Figure(figsize=(5, 2), dpi=100)
+        plot = fig.add_subplot(1, 1, 1)
+        # Sample data for discrete points
+        x = [1, 2, 3, 4, 5]
+        y = [2, 3, 5, 7, 11]
+
+        plot.scatter(x, y, color="blue", marker="x")  # Discrete points with circular markers
+        plot.set_xlabel("X-axis")
+        plot.set_ylabel("Y-axis")
+        plot.set_title("Discrete Points Plot")
+
+        # Embed the figure into the Tkinter canvas
+        canvas = FigureCanvasTkAgg(fig, master=frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        fig = Figure(figsize=(5, 2), dpi=100)
+        plot = fig.add_subplot(1, 1, 1)
+        plot.plot([1, 2, 3, 4, 5], [1, 4, 2, 5, 3])  # Example data
+
+        canvas = FigureCanvasTkAgg(fig, master=frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
+
+        print("in display graphs")
+    else:
+        error_lbl.place(x=200, y=300)
+
+
+def generate_signal_input(root):
+    
+    frame = right_frame(root)
+    #label for trigerring errors 
+    error_label = tk.Label(frame, text="enter a valid input")
+    # error_label.place(x=200,y=300)
 
     for i, (text, pos) in enumerate(zip(labels_text, positions)):
         label = tk.Label(frame, text=text.lower(), bg="#d3d3d3")
@@ -51,7 +110,7 @@ def generate_signal(root):
             entries[text] = entry
 
     # Display Signal Button
-    display_button = tk.Button(frame, text="Display Signal", bg="#808080", fg="white", width=15, height=2)
+    display_button = tk.Button(frame, text="Display Signal", bg="#808080", fg="white", width=15, height=2, command=lambda:display_graph(error_label, frame, root))
     display_button.place(x=380, y=400)
 
 
@@ -61,8 +120,8 @@ def main_window():
     root.configure(bg="#d3d3d3")
     root.title("Signal Generator")
 
-    left_section(root)
-    generate_signal(root)
+    right_frame = sections(root)
+    generate_signal_input(right_frame)
 
     root.mainloop()
 
--- utils.py
+++ utils.py
@@ -5,10 +5,37 @@ class Button:
         self.name = name
         self.onClick = onClick
 
-
+class Signal:
+     def __init__(self, amp, phase, analog_freq, sampling_freq, sample_no, func):
+        self.amp = amp
+        self.phase = phase
+        self.analog_freq = analog_freq
+        self.sampling_freq = sampling_freq
+        self.sample_no = sample_no
+        self.func = func
+         
 #check if all field are entered by the user
-def valid_inputs(entries, labels):
-    for lbl in labels:
-        if entries[lbl] == None:
+#TODO -> check for each constraint [fs range and so on]
+def valid_inputs(entries, error_lbl):
+    error_lbl.place(x=200, y=700)
+    print("display signal btn is pressed")
+    for key, value in zip(entries.keys(), entries.values()):
+        print(value.get())
+        if not value.get() or (key=="Signal Generator" and value.get()=="choose function"):
             return False
     return True
+
+def is_float(value):
+    try:
+        float(value)  
+        return True  
+    except ValueError:
+        return False  
+
+
+def is_int(value):
+    try:
+        int(value)  
+        return True  
+    except ValueError:
+        return False  
